b16fe0f322fc3951dbbf853a300767541cc7c63a
iDiego <me@diegor.nl>
2021-03-26 02:59:07 +0100

    Create Switch homepage [ch202] (#2)
    * Install Debugbar & Ray packages
    
    * Create Switch homepage [ch202]
    
    * Import favicons & other meta stuff

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,14 +4,45 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @hasSection('title')
-
             <title>@yield('title') - {{ config('app.name') }}</title>
         @else
             <title>{{ config('app.name') }}</title>
         @endif
 
         <!-- Favicon -->
-		<link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
+        <link rel="apple-touch-icon" sizes="180x180"
+              href="{{ asset('img/favicons/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32"
+              href="{{ asset('img/favicons/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16"
+              href="{{ asset('img/favicons/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+        <link rel="mask-icon" href="{{ asset('img/favicons/safari-pinned-tab.svg') }}" color="#5bbad5">
+        <link rel="shortcut icon" href="{{ asset('img/favicons/favicon.ico') }}">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="msapplication-config" content="{{ asset('browserconfig.xml') }}">
+        <meta name="theme-color" content="#18181B">
+
+        <!-- Open Graph / Facebook Meta Tags -->
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="{{ route('home') }}">
+        <meta property="og:title" content="@yield('title') - {{ config('app.name') }}">
+        <meta property="og:description"
+              content="Phoenix Switch will help drivers migrate their jobs from TrucksBook to the PhoenixBase.">
+        <meta property="og:locale" content="en_GB">
+        <meta property="og:image"
+              content="{{ asset('img/meta-image.png') }}">
+
+        <!-- Twitter Meta Tags -->
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="{{ route('home') }}">
+        <meta property="twitter:title" content="@yield('title') - {{ config('app.name') }}">
+        <meta property="twitter:description"
+              content="Phoenix Switch will help drivers migrate their jobs from TrucksBook to the PhoenixBase.">
+        <meta property="twitter:site" content="@PhoenixVTC">
+        <meta property="twitter:creator" content="@PhoenixVTC">
+        <meta property="twitter:image"
+              content="{{ asset('img/meta-image.png') }}">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
@@ -22,6 +53,7 @@
 
         <!-- Scripts -->
         <script src="{{ url(mix('js/app.js')) }}" defer></script>
+        @stack('scripts')
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
