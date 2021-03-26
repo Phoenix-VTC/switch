e7dfab86e5e73ea24bc65f690b6d0bebea359794
iDiego <me@diegor.nl>
2021-03-26 11:26:03 +0100

    Remove default migrations [ch203] (#3)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    /**
+     * The database connection that should be used by the model.
+     *
+     * @var string
+     */
+    protected $connection = 'base';
 
     /**
      * The attributes that are mass assignable.
