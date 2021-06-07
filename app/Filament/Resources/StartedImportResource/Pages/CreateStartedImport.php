4b2958c65a9109b08d5d79dfd2a5792950ee688c
Diego <me@diegor.nl>
2021-06-07 14:13:18 +0200

    Create StartedImport Resource [ch333]

---
+++ app/Filament/Resources/StartedImportResource/Pages/CreateStartedImport.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\StartedImportResource\Pages;
+
+use App\Filament\Resources\StartedImportResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateStartedImport extends CreateRecord
+{
+    public static $resource = StartedImportResource::class;
+}
---
+++ app/Filament/Resources/StartedImportResource/Pages/EditStartedImport.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\StartedImportResource\Pages;
+
+use App\Filament\Resources\StartedImportResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditStartedImport extends EditRecord
+{
+    public static $resource = StartedImportResource::class;
+}
